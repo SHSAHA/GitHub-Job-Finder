3979554125cf2a0da0ee8afc016dd5858e6099a4
Shaswata Saha <shaswatasaha24@example.com>
2020-08-30 13:29:07 +0530

    hot reloading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import useFetchJobs from './customHook/useFetchJobs';
 
 const App = () => {
-	return <div>app</div>;
+	const { loading, jobs, error } = useFetchJobs();
+	return (
+		<div>
+			{loading && <h1>Loading...</h1>}
+			{error && <h1>Error. Try Refreshing.</h1>}
+			<h1>{jobs.length}</h1>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/customHook/useFetchJobs.js
@@ -0,0 +1,7 @@
+export default function useFetchJobs() {
+	return {
+		loading: true,
+		jobs: [ 1, 2, 3 ],
+		error: false
+	};
+}
